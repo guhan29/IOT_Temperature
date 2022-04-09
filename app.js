4dd25b8fdd03c09d78f3222725f43359a97117b9
Guhan M <mguhan439@gmail.com>
2022-04-09 23:19:59 +0530

    modified code

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
+// basic setup requirements
 var express = require('express');
 var bodyParser = require('body-parser');
 var compress = require('compression');
-var renderer = require('./routes/renderer');
-var cors = require('cors');
 
 var app = express();
-app.use(cors());
+
+var renderer = require('./routes/renderer');
+
 app.use(compress());
 app.use(bodyParser.json({limit: '100mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', renderer);
 
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     var html = '<!DOCTYPE html>';
@@ -28,7 +32,6 @@ app.use(function(err, req, res, next) {
     html+= '  <body>';
     html+= '    <h1>'+err.message+'</h1>';
     html+= '    <h2>'+err.status+'</h2>';
-    html+= '    <h2>Hello, World</h2>';
     html+= '    <pre>'+err.stack+'</pre>';
     html+= '  </body>';
     html+= '</html>';
--- routes/renderer.js
+++ routes/renderer.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-	return res.send("<h1>Hello World from Openode!</h1>");
+	return res.send("Hello opeNode!");
 });
 
-router.get('/:value', function (req, res) {
-	return res.send("<h1>Your request GET /"+req.params.value+"</h1>");
-});
+
 
 module.exports = router;
